d6a2e2a0b85f456a58b8599a936b34615fe707e8
65456u <yanghongchen@bupt.edu.cn>
2024-05-11 19:43:38 +0800

    arranged data model

--- acm/data.py
+++ acm/data.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
@@ -7,9 +9,69 @@ class Settings(BaseModel):
     mode: str
 
 
+class CheckInRequest(BaseModel):
+    user_id: str
+
+
+class CheckInResponse(BaseModel):
+    room_id: int
+
+
 class Invoice(BaseModel):
     room_id: str
     start_time: str
     end_time: str
     settings: Settings
     cost: float
+
+
+class ReportItem(BaseModel):
+    room_id: int
+    status: int
+    settings: Settings
+
+
+class ReportResponse(BaseModel):
+    reports: List[Invoice]
+
+
+class CheckoutRequest(BaseModel):
+    user_id: str
+
+
+class CheckoutResponse(BaseModel):
+    cost: float
+    invoices: List[Invoice]
+
+
+class RoomRequest(BaseModel):
+    room_id: int
+
+
+class ACSwitchResponse(BaseModel):
+    status: bool
+    settings: Settings
+
+
+class ACSettingRequest(BaseModel):
+    room_id: int
+    settings: Settings
+
+
+class RoomCostResponse(BaseModel):
+    cost: float
+
+
+class RoomStatusResponse(BaseModel):
+    status: bool
+    settings: Settings
+
+
+class UserRegisterRequest(BaseModel):
+    name: str
+    phone: str
+    password: str
+
+
+class UserRegisterResponse(BaseModel):
+    user_id: str
--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-from typing import List
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 from acm import Manager
-from acm.data import Invoice, Settings
+from acm.data import *
 
 app = FastAPI()
 manager = Manager()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-class UserRegisterRequest(BaseModel):
-    name: str
-    phone: str
-    password: str
-
-
-class UserRegisterResponse(BaseModel):
-    user_id: str
 
 
 @app.post("/user/register", response_model = UserRegisterResponse)
@@ -38,29 +15,12 @@ async def register_user(user_request: UserRegisterRequest):
     return {"user_id": user_id}
 
 
-class CheckInRequest(BaseModel):
-    user_id: str
-
-
-class CheckInResponse(BaseModel):
-    room_id: int
-
-
 @app.post("/checkin", response_model = CheckInResponse)
 def check_in(check_in_request: CheckInRequest):
     # 将签到逻辑放在这里，包括根据用户ID获取房间ID等操作
     # 返回包含房间ID的响应
     room_id = 1  # 替换为实际的房间ID生成逻辑
     return {"room_id": room_id}
-
-
-class CheckoutRequest(BaseModel):
-    user_id: str
-
-
-class CheckoutResponse(BaseModel):
-    cost: float
-    invoices: List[Invoice]
 
 
 @app.post("/checkout", response_model = CheckoutResponse)
@@ -75,15 +35,6 @@ def checkout(checkout_request: CheckoutRequest):
                 settings = Settings(temperature = 22, fan_speed = "Low", mode = "Heat"), cost = 50.0)
     ]  # 替换为实际的详单生成逻辑
     return {"cost": cost, "invoices": invoices}
-
-
-class RoomRequest(BaseModel):
-    room_id: int
-
-
-class ACSwitchResponse(BaseModel):
-    status: bool
-    settings: Settings
 
 
 @app.post("/ac/on", response_model = ACSwitchResponse)
@@ -106,11 +57,6 @@ def turn_off_ac(ac_off_request: RoomRequest):
     return {"status": status, "settings": settings}
 
 
-class ACSettingRequest(BaseModel):
-    room_id: int
-    settings: Settings
-
-
 @app.post("/ac/settings", response_model = ACSwitchResponse)
 def set_ac(ac_set_request: ACSettingRequest):
     # 设置空调参数逻辑放在这里，包括根据房间ID和参数执行相应操作
@@ -121,33 +67,14 @@ def set_ac(ac_set_request: ACSettingRequest):
     return {"status": status, "settings": settings}
 
 
-class RoomCostResponse(BaseModel):
-    cost: float
-
-
 @app.post("/ac/cost", response_model = RoomCostResponse)
 def get_cost(room_query_request: RoomRequest):
     return {"cost": 100.0}
 
 
-class RoomStatusResponse(BaseModel):
-    status: bool
-    settings: Settings
-
-
 @app.get("/ac/status", response_model = RoomStatusResponse)
 def get_ac_status(room_query_request: RoomRequest):
     return {"status": True, "settings": Settings(temperature = 25, fan_speed = "High", mode = "Cool")}
-
-
-class ReportItem(BaseModel):
-    room_id: int
-    status: int
-    settings: Settings
-
-
-class ReportResponse(BaseModel):
-    reports: List[Invoice]
 
 
 @app.get("/ac/reports", response_model = ReportResponse)
